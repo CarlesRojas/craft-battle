2518b24da528a46751fda4fba003f8ea34c0cee0
CarlesRojas <carlesrojas@outlook.com>
2025-11-06 12:52:31 +0100

    normalized

--- convex/combination.ts
+++ convex/combination.ts
@@ -8,13 +8,13 @@ export const get = query({
         word2: v.string(),
     },
     handler: async (ctx, { word1, word2 }) => {
-        const normalizedWord1 = normalize(word1, true)
-        const normalizedWord2 = normalize(word2, true)
-        const sortedWords = [normalizedWord1, normalizedWord2].sort()
+        const sortedWords = [word1, word2].sort()
+        const word1Normalized = normalize(sortedWords[0], true)
+        const word2Normalized = normalize(sortedWords[1], true)
 
         return await ctx.db
             .query('combination')
-            .withIndex('words', q => q.eq('word1Normalized', sortedWords[0]).eq('word2Normalized', sortedWords[1]))
+            .withIndex('words', q => q.eq('word1Normalized', word1Normalized).eq('word2Normalized', word2Normalized))
             .first()
     },
 })
@@ -28,24 +28,32 @@ export const create = mutation({
         depth: v.optional(v.number()),
     },
     handler: async (ctx, args) => {
-        const normalizedWord1 = normalize(args.word1, true)
-        const normalizedWord2 = normalize(args.word2, true)
-        const sortedWords = [normalizedWord1, normalizedWord2].sort()
+        const sortedWords = [args.word1, args.word2].sort()
+        const word1Normalized = normalize(sortedWords[0], true)
+        const word2Normalized = normalize(sortedWords[1], true)
 
         let newDepth = args.depth ?? 0
 
         if (args.depth === undefined) {
             const word1Combination = await ctx.db
                 .query('combination')
-                .withIndex('result', q => q.eq('resultNormalized', sortedWords[0]))
-                .first()
+                .withIndex('result', q => q.eq('resultNormalized', word1Normalized))
+                .collect()
+            const minWord1Depth = word1Combination.reduce(
+                (min, combination) => Math.min(min, combination.depth),
+                Number.MAX_SAFE_INTEGER,
+            )
 
             const word2Combination = await ctx.db
                 .query('combination')
-                .withIndex('result', q => q.eq('resultNormalized', sortedWords[1]))
-                .first()
+                .withIndex('result', q => q.eq('resultNormalized', word2Normalized))
+                .collect()
+            const minWord2Depth = word2Combination.reduce(
+                (min, combination) => Math.min(min, combination.depth),
+                Number.MAX_SAFE_INTEGER,
+            )
 
-            newDepth = Math.max(word1Combination?.depth ?? 0, word2Combination?.depth ?? 0) + 1
+            newDepth = Math.max(minWord1Depth, minWord2Depth) + 1
         }
 
         return await ctx.db.insert('combination', {
@@ -54,8 +62,8 @@ export const create = mutation({
             word1: sortedWords[0],
             word2: sortedWords[1],
 
-            word1Normalized: normalizedWord1,
-            word2Normalized: normalizedWord2,
+            word1Normalized,
+            word2Normalized,
             resultNormalized: normalize(args.result, true),
         })
     },

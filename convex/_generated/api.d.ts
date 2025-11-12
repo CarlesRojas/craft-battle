d3a84b4f0487788db71c62cd2eb61d1d59594694
CarlesRojas <carlesrojas@outlook.com>
2025-11-12 13:44:26 +0100

    remove old hook

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,19 +8,15 @@
  * @module
  */
 
-import type * as bingo from "../bingo.js";
-import type * as classic from "../classic.js";
-import type * as combination from "../combination.js";
-import type * as instance from "../instance.js";
-import type * as inviteBingo from "../inviteBingo.js";
-import type * as username from "../username.js";
-import type * as word from "../word.js";
+import type * as bingo from '../bingo.js'
+import type * as classic from '../classic.js'
+import type * as combination from '../combination.js'
+import type * as instance from '../instance.js'
+import type * as inviteBingo from '../inviteBingo.js'
+import type * as username from '../username.js'
+import type * as word from '../word.js'
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
+import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server'
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -31,23 +27,17 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  bingo: typeof bingo;
-  classic: typeof classic;
-  combination: typeof combination;
-  instance: typeof instance;
-  inviteBingo: typeof inviteBingo;
-  username: typeof username;
-  word: typeof word;
-}>;
-declare const fullApiWithMounts: typeof fullApi;
+    bingo: typeof bingo
+    classic: typeof classic
+    combination: typeof combination
+    instance: typeof instance
+    inviteBingo: typeof inviteBingo
+    username: typeof username
+    word: typeof word
+}>
+declare const fullApiWithMounts: typeof fullApi
 
-export declare const api: FilterApi<
-  typeof fullApiWithMounts,
-  FunctionReference<any, "public">
->;
-export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
-  FunctionReference<any, "internal">
->;
+export declare const api: FilterApi<typeof fullApiWithMounts, FunctionReference<any, 'public'>>
+export declare const internal: FilterApi<typeof fullApiWithMounts, FunctionReference<any, 'internal'>>
 
-export declare const components: {};
+export declare const components: {}
